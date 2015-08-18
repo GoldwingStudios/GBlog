acdebbe3843619ca4290df0e5f8080cfb22f6e15
GoldwingStudios <ocd@goldwingstudios.de>
2015-08-18 18:18:00 +0200

    RESET

--- administration/backend/classes/User_Roles.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class User_Roles extends Enumerations {
-
-    const Administrator = 1;
-    const Account_Manager = 2;
-    const Blog_Author = 3;
-
-}
--- administration/modules/admin_functions/functions/delete_installer.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-if (file_exists("../../../installer.php")) {
-    unlink("../../../installer.php");
-}
-
-header('Location: ../../../index.php');
-exit;
